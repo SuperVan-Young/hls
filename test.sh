778da8991c1ef0c52f69378c4be845fdb1fcab0a
Chenhao Xue <xch927027@pku.edu.cn>
2022-05-17 19:16:14 +0800

    add testing submodule

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-for test_case in $(ls ./cases | grep test_case[1-9].txt)
+# a basic checking
+for test_case in $(ls ./HLS-lab1/cases | grep test_case[1-9].txt)
 do
     echo ${test_case}
-    ./build/display "./cases/${test_case}" > "./results/input_${test_case}"
-    ./build/hls "./cases/${test_case}" > "./results/${test_case}"
-    ./build/checker "./cases/${test_case}" "./results/${test_case}"
+    ./build/display ./HLS-lab1/cases/${test_case} > ./results/input_${test_case}
+    ./build/hls ./HLS-lab1/cases/${test_case} > ./results/${test_case}
     echo ""
+done
+
+# thorough checking
+cd ./HLS-lab1/cases
+
+for i in [1..8]
+do
+    ./test.sh ${i} ../../build/hls ../build/src/checker ../build/src/score_calc
 done
